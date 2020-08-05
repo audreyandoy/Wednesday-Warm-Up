8b443dd14040bd150e769040010a4b2e465b6aed
Audrey Andoy <audreylandoy@gmail.com>
2020-08-05 19:24:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+#VARIABLE - something that can store
+# information
+name = "Spongebob"
+score = 0
+height = 62.5
+# Boolean data type
+# State variable
+game_over = True
+
+# LOOPS - repeats code 
+
+# for loop - repeats code for a given amount of times
+
+for i in range(4):
+  print("I will slow down today")
+
+# while loop - repeats code until its condition is met aka turns False
+count = 0
+while count < 3:
+  print("slow down", count)
+  count += 1
+
+
+  
+
+
